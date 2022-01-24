91a370f96c6979f774edde7e8fae56e10900985d
hynesnd <hynesnd@gmail.com>
2022-01-24 22:55:01 +0000

    Adds some colours from colorhunt

---
+++ src/Colors.elm
@@ -0,0 +1,43 @@
+module Colors exposing (..)
+
+import Element exposing (rgb255)
+
+
+darkNavy =
+    rgb255 26 26 64
+
+
+lightNavy =
+    rgb255 39 0 130
+
+
+purple =
+    rgb255 122 11 192
+
+
+pink =
+    rgb255 250 88 182
+
+
+black =
+    rgb255 23 23 23
+
+
+darkGrey =
+    rgb255 68 68 68
+
+
+red =
+    rgb255 218 0 55
+
+
+offWhite =
+    rgb255 237 237 237
+
+
+ffUlBlue =
+    rgb255 0 0 176
+
+
+ffLrBlue =
+    rgb255 0 0 32
